f04f7b0af818d591d6f6218914fe741192ffcf50
voltah <voltah@voltah-Nitro-ANV15-41>
2026-04-25 17:30:49 +0800

    Added spring libs

--- DLS/src/lms/DLS_main.java
+++ DLS/src/lms/DLS_main.java
@@ -1,8 +1,11 @@
 package lms;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class DLS_main {
-
 	public static void main(String[] args) {
+		SpringApplication.run(DLS_main.class, args);
 		System.out.println("Hello DLS!");
 
 	}
